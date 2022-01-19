f60fdf566f00c06dedef4d8d7f871baf369f1e68
Marco Köllner <it@mk-swe.de>
2022-01-19 21:29:08 +0100

    Add tailwind css

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $titleTag ?></title>
     <meta name="description" content="<?= $metaDescription ?>">
+
+    <?= css('assets/css/style.css') ?>
 </head>
 
 <body>
